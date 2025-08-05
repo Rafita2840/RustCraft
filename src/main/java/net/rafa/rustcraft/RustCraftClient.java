0e0097c9e1c529c61d16a71a523c3c623a052ea6
Rafael <rafita2855000@gmail.com>
2025-08-05 17:18:46 +0100

    Initial commit

---
+++ src/main/java/net/rafa/rustcraft/RustCraftClient.java
@@ -0,0 +1,11 @@
+package net.rafa.rustcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RustCraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/rafa/rustcraft/RustCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rafa.rustcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RustCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
