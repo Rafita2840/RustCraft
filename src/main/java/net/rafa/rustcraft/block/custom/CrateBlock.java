dba26586646de929cef0804e06fcb1a5046c006a
Rafael <rafita2855000@gmail.com>
2025-09-22 07:40:33 +0100

    Fixed bugs

--- src/main/java/net/rafa/rustcraft/block/custom/CrateBlock.java
+++ src/main/java/net/rafa/rustcraft/block/custom/CrateBlock.java
@@ -9,10 +9,7 @@ import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
@@ -43,17 +40,17 @@ public class CrateBlock extends BlockWithEntity implements BlockEntityProvider {
         return BlockRenderType.MODEL;
     }
 
-//    @Override
-//    protected void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
-//        if (state.getBlock() != newState.getBlock()){
-//            BlockEntity blockEntity = world.getBlockEntity(pos);
-//            if (blockEntity instanceof CrateBlockEntity){
-//                ItemScatterer.spawn(world, pos, (Inventory) blockEntity);
-//                world.updateComparators(pos, this);
-//            }
-//            super.onStateReplaced(state, world, pos, newState, moved);
-//        }
-//    }
+    @Override
+    protected void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (state.getBlock() != newState.getBlock()){
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+            if (blockEntity instanceof CrateBlockEntity){
+                ItemScatterer.spawn(world, pos, (Inventory) blockEntity);
+                world.updateComparators(pos, this);
+            }
+            super.onStateReplaced(state, world, pos, newState, moved);
+        }
+    }
 
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
